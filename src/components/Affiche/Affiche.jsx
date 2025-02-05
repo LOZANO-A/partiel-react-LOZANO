1c43fb588380b5d3a2830f3a6ff463f070bc973b
LOZANO-A <anthony.lozano@student.junia.com>
2025-02-05 16:49:07 +0100

    semblant_de_component

--- src/components/Affiche/Affiche.jsx
+++ src/components/Affiche/Affiche.jsx
@@ -4,9 +4,18 @@ function Affiche()
 {
 
     return(
-        <>
-
-        </>
+        <div className="movie">
+            <picture>
+                <img src={film.img} alt={film.titre} title={film.title}/>
+            </picture>
+            <figcaption>
+                <div className="Score">
+                    <p>{film.score}</p>
+                </div>
+                <h5>{film.title}</h5>
+                <p>{film.date}</p>
+            </figcaption>
+        </div>
     )
 }
 
---
+++ src/components/detail/DetailFilm.jsx
@@ -0,0 +1,38 @@
+import "DetailFilm.scss"
+
+function DetailFilm()
+{
+
+    return(
+        <div className="Detail">
+            <div className="Content">
+                <picture>
+                    <img src={film.img} alt={film.titre} title={film.title}/>
+                </picture>
+                <div className="Top">
+                    <div className="Score">
+                        <p>{film.score}</p>
+                    </div>
+                    <div className="TitleDate">
+                        <h2>{film.title}</h2>
+                        <span>{film.date} - {film.genre} - {film.duree}</span>
+                    </div>
+                </div>
+                <div  className="Synopsis">
+                    <h2>Synopsis</h2>
+                    <p>{film.sysnopsis}</p>
+                </div>
+            </div>
+            <div className="Cast">
+                <h2>Casting</h2>
+                <div className="Actor">
+                    <img src={actor.img} alt={actor.name}/>
+                    <h4>{actor.name}</h4>
+                    <span>{actor.role}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default  DetailFilm
